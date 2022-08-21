8bc248e498242e14cdaf9cf1d6b62fe707a017cd
mfarzamalam <mfarzamalam@gmail.com>
2022-08-22 04:28:31 +0500

    added model for opt verification

--- twilioapp/models.py
+++ twilioapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class OTPVerification(models.Model):
+    username=models.CharField(max_length=150, default=0)
+    otp = models.IntegerField(default=-1)
+
+    def __str__(self):
+        return self.name           
